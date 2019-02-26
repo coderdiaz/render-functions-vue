b763a274ceab7c474b4e28132cc7c0aefe77bb23
Javier Diaz <hello@coderdiaz.me>
2019-02-25 18:03:19 -0600

    feat: added base-component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,51 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <ul class="filter__list">
+      <li class="filter__item"><a @click.prevent="changeFilter('USD')" href="#">USD</a></li>
+      <li class="filter__item"><a @click.prevent="changeFilter('MXN')" href="#">MXN</a></li>
+      <li class="filter__item"><a @click.prevent="changeFilter('COP')" href="#">COP</a></li>
+    </ul>
+    <PaymentList :currency="currency"></PaymentList>
+  </div>
 </template>
 
 <script>
+import PaymentList from '@/components/PaymentList.vue';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      currency: 'USD',
+    };
+  },
+  methods: {
+    changeFilter(type) {
+      this.currency = type;
+    },
+  },
+  components: {
+    PaymentList,
+  },
 };
 </script>
+
+<style>
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  text-align: center;
+}
+
+.filter__list > .filter__item {
+  list-style: none;
+  padding: 5px 0;
+  display: inline-flex;
+  margin-right: 10px;
+}
+
+.filter__list > .filter__item a {
+  text-decoration: none;
+  color: #1e2fbb;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/PaymentList.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul class="payment__list">
+    <li
+      class="payment__item"
+      v-for="(payment, index) in payments"
+      :key="index">
+        <span>Price:</span> {{ payment.price }} {{ payment.currencyType }}
+        <span>Payee:</span> {{ payment.payee }}
+      </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'PaymentList',
+  props: {
+    currency: {
+      type: String,
+      default: 'USD',
+    },
+  },
+  data() {
+    return {
+      payments: [
+        {
+          name: 'Cookies', price: 3.00, currencyType: 'USD', payee: 'Platzi Commerce',
+        },
+        {
+          name: 'Soda', price: 4.00, currencyType: 'MXN', payee: 'Platzi Commerce',
+        },
+        {
+          name: 'Cookies', price: 3.00, currencyType: 'USD', payee: 'Platzi Commerce',
+        },
+        {
+          name: 'Chocolate Cookies', price: 5.00, currencyType: 'COP', payee: 'Platzi Commerce',
+        },
+        {
+          name: 'Ice Cream', price: 6.00, currencyType: 'COP', payee: 'Platzi Commerce',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.payment__list > .payment__item {
+  list-style: none;
+  padding: 5px 0;
+}
+
+.payment__list > .payment__item span {
+  font-weight: bold;
+  text-transform: uppercase;
+}
+</style>
